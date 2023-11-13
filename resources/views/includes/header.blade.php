490cca659e5005fe1141cdb495986ae1f404241d
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2023-11-13 08:39:05 -0300

    Adicionando botao logout no cabecalho

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,8 +15,14 @@
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          <li>
+            <a onclick="$('#logout-form').submit()">
+              <span class="glyphicon glyphicon-user">
+                <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                  @csrf
+                </form>
+              </span> Log out</a>
+          </li>
     </ul>
   </div>
 </nav>
